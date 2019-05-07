d297127505799e566e93dfb64346d526b3922084
Pengcheng Chen <pengcheng.chen@gmail.com>
2019-05-06 18:03:10 -0700

    Revert "Removed package config"
    This reverts commit b17d7b36bd5dc892c0431ba0b46f49cefc928a6b.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="leetcode-cli",
+    version="0.0.5",
+    author="Pengcheng Chen",
+    author_email="pengcheng.chen@gmail.com",
+    description="LeetCode CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chenpengcheng/cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="leetcode",
+    include_package_data=True,
+    install_requires=[
+        "ascii_graph",
+        "bs4",
+        "PyExecJS",
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "leetcode-cli=leetcodecli.cli:main",
+        ],
+    },
+)
